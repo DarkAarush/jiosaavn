b8ee25b0b8916f4d4514efc5e3605827c8a12135
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2023-04-29 08:19:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://static.apkdone.me/wp-content/uploads/2020/10/Wynk-Music-poster.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://images.app.goo.gl/j7Eq9WyfZB9qKA1t6")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
@@ -135,4 +135,4 @@ if PING_IMG_URL:
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://static.apkdone.me/wp-content/uploads/2020/10/Wynk-Music-poster.jpg"
+            START_IMG_URL = "https://images.app.goo.gl/j7Eq9WyfZB9qKA1t6"
