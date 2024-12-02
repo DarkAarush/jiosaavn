a0c7e05b2a32164d666cca0dc6dd94c73c362647
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2024-12-02 05:58:10 +0530

    Update __init__.py

--- AnonX/__init__.py
+++ AnonX/__init__.py
@@ -2,7 +2,7 @@ from AnonX.core.bot import AnonXBot
 from AnonX.core.dir import dirr
 from AnonX.core.git import git
 from AnonX.core.userbot import Userbot
-from AnonX.misc import dbb, heroku, sudo
+from AnonX.core.misc import dbb, heroku
 from aiohttp import ClientSession
 
 from .logging import LOGGER
